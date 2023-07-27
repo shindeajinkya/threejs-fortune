f20c405918571e06f97d2f01d9cbf1e22d2d7432
ajinkya9 <ajinkya@zomentum.com>
2023-07-27 18:33:42 +0530

    fix: keep it 1

--- src/script.js
+++ src/script.js
@@ -129,8 +129,8 @@ tick();
 if ("ondeviceorientation" in window) {
   window.addEventListener("deviceorientation", (event) => {
     const { alpha, beta, gamma } = event;
-    camera.position.x += (gamma / 100 - camera.position.x) * 0.8;
-    camera.position.y += (-beta / 100 - camera.position.y) * 0.8;
+    camera.position.x += (gamma / 100 - camera.position.x) * 1;
+    camera.position.y += (-beta / 100 - camera.position.y) * 1;
   });
 }
 
